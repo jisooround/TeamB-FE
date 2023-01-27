ff8bbb9fcc75cca744a69f70ff1de15b8909348d
jisooround <9utesleben@gmail.com>
2023-01-27 18:04:43 +0900

    Feat: header components

---
+++ src/components/layout/header.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const Header = () => {
+  return <Wrap>Header</Wrap>;
+};
+
+const Wrap = styled.div`
+  width: 100%;
+  height: 100px;
+  background-color: #f8be00;
+`;
+export default Header;
