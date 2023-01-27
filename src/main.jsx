4519940b3e7945fe21163d790851d988db1a53b0
jisooround <9utesleben@gmail.com>
2023-01-27 18:05:16 +0900

    Feat: children(Search)

--- src/main.jsx
+++ src/main.jsx
@@ -2,14 +2,19 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import App from './App';
-import './index.css';
+import Home from './pages/Home';
+import Search from './pages/Search';
+import NotFound from './pages/Common/NotFound';
 
 const router = createBrowserRouter([
   {
     path: '/',
     element: <App />,
-    //errorElement: <NotFound />,
-    //children: [{ index: true, path: '/', element: <MainPage /> }],
+    errorElement: <NotFound />,
+    children: [
+      { index: true, path: '/', element: <Home /> },
+      { path: '/search', element: <Search /> },
+    ],
   },
 ]);
 
