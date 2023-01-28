623bf201b8c1a3320c00655d94f26a381717d128
mineclover <mineclover@naver.com>
2023-01-29 04:50:14 +0900

    feat : flex columns

---
+++ src/pages/detail/SecondSection.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const Nav = styled.nav`
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  align-items: flex-start;
+  padding: 0px;
+
+  width: 100%;
+  height: 32px;
+`;
+
+const index = () => {
+  return <Nav> </Nav>;
+};
+
+export default index;
--- src/pages/detail/index.jsx
+++ src/pages/detail/index.jsx
@@ -1,8 +1,38 @@
 import React from 'react';
-import Header from '@components/layout/header';
+import styled from 'styled-components';
+import FirstSection from './FirstSection';
+import SecondSection from './SecondSection';
+
+const DetailRoot = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  padding: 0px;
+  width: 100%;
+  position: relative;
+  background: #ffffff;
+`;
+
+const SectionWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+
+  width: 100%;
+  max-width: 1136px;
+  padding-top: 32px;
+`;
 
 const index = () => {
-  return <Header />;
+  return (
+    <DetailRoot>
+      <SectionWrapper>
+        <FirstSection />
+        <Test />
+        <SecondSection />
+      </SectionWrapper>
+    </DetailRoot>
+  );
 };
 
 export default index;
