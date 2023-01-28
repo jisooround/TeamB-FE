6ba67311e0e10f170fa52e35a0fb764e746dc8ca
mineclover <mineclover@naver.com>
2023-01-28 21:35:48 +0900

    rename : global > styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { RecoilRoot } from 'recoil';
 import { Outlet } from 'react-router-dom';
 import Header from './components/layout/Header';
-import GlobalStyles from './global/globalStyles';
+import GlobalStyles from '@styles/globalStyles';
 
 function App() {
   return (
