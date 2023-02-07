428630f0e7cd7789c3413ff167b59ebb0c956819
jisooround <9utesleben@gmail.com>
2023-02-07 20:26:02 +0900

    Feat: getDetails func

--- src/api/api.js
+++ src/api/api.js
@@ -1,23 +1,20 @@
 import { instance } from './axios';
+import { AxiosError } from 'axios';
+import { detailDummydata } from '@/data/dummydata';
 
-// export const getDetails = async (id, setState, setError) => {
-//   try {
-//     const res = await instance.get(`/commentThreads?part=snippet&videoId=${id}`);
+export const getDetails = async (id, setState, setError) => {
+  try {
+    const res = await instance.get(`tourlist/detail?contentid=${id}`);
 
-//     if (res.status === 200) {
-//       setState(res);
-//     }
-//   } catch (error) {
-//     if (error instanceof AxiosError) {
-//       console.log(error.message);
-//       setError(error.message);
-//     }
-//   }
-// };
-
-export const getDetails = async (detailId) => {
-  const sending = {
-    contentid: detailId,
-  };
-  return instance.get(`tourList/detail`, { params: { ...sending } });
+    if (res.status === 200) {
+      setState(res);
+      console.log(res);
+    }
+  } catch (error) {
+    if (error instanceof AxiosError) {
+      console.log(error.message);
+      setError(error.message);
+      setState(detailDummydata);
+    }
+  }
 };
