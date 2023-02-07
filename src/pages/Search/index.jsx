a26e1ac0091f00e26bd4fe7fc65af0faf4fd68ba
soyun <soyoon4122@naver.com>
2023-02-08 03:40:43 +0900

    Feat: search prams & api connect

--- src/pages/Search/index.jsx
+++ src/pages/Search/index.jsx
@@ -1,42 +1,70 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import styled from 'styled-components';
-import { searchListDummyData } from '@/data/dummydata';
+import { FcInfo } from 'react-icons/fc';
+import { getKeyword } from '@api/api';
 import SearchCard from '@components/search/SearchCard';
 import PageTitle from '@components/common/PageTitle';
 import Pagination from '@components/common/pagination/Pagination';
-import { useParams } from 'react-router-dom';
+import { useLocation, useSearchParams } from 'react-router-dom';
 
 const Search = () => {
-  const { search } = useParams;
+  const [keywordParams, setKeywordParams] = useSearchParams();
   const [page, setPage] = useState(1);
   const limit = 12;
   const offset = (page - 1) * limit;
-  console.log(searchListDummyData);
+  const [searchList, setSearchList] = useState([]);
+  const [total, setTotal] = useState(0);
+  console.log(searchList);
+  console.log(keywordParams);
+
+  const useQuery = () => {
+    return new URLSearchParams(useLocation().search);
+  };
+
+  let query = useQuery();
+  const searchKeyword = query.get('keyword');
+  console.log(searchKeyword);
+
+  useEffect(() => {
+    if (searchKeyword) {
+      getSearchKeyword(page, searchKeyword);
+    }
+  }, [searchKeyword]);
+
+  const getSearchKeyword = async () => {
+    try {
+      const response = await getKeyword(page, searchKeyword);
+      setTotal(response.data.data.totalElements);
+      setSearchList(response.data.data.content);
+    } catch (error) {
+      console.log(error);
+    }
+  };
 
   return (
     <Container>
       <PageTitle title={'검색 결과'} />
-      {Array.isArray(searchListDummyData) ? (
+      {searchList.length > 0 ? (
         <>
-          <h1>{search} 에 대한 검색 결과입니다.</h1>
+          <h1>
+            <span>{searchKeyword}</span>에 대한 검색 결과입니다.
+          </h1>
           <Inner>
-            {searchListDummyData.slice(offset, offset + limit).map((list) => {
-              return <SearchCard list={list} />;
+            {searchList.slice(offset, offset + limit).map((list, idx) => {
+              return <SearchCard list={list} key={idx} />;
             })}
           </Inner>
         </>
       ) : (
         <NoList>
-          <h1>{search} 에 대한 검색 결과가 없습니다.</h1>
+          <FcInfo size="40" />
+          <h1>
+            <span>{searchKeyword}</span>에 대한 검색 결과가 없습니다.
+          </h1>
         </NoList>
       )}
-      {Array.isArray(searchListDummyData) ? (
-        <Pagination
-          total={searchListDummyData.length}
-          limit={limit}
-          page={page}
-          setPage={setPage}
-        />
+      {searchList.length > 0 ? (
+        <Pagination total={total} limit={limit} page={page} setPage={setPage} />
       ) : null}
     </Container>
   );
@@ -50,6 +78,9 @@ const Container = styled.div`
     font-size: 22px;
     font-weight: bold;
     color: #464646;
+    span {
+      color: #2358c5;
+    }
   }
 `;
 
@@ -59,12 +90,28 @@ const Inner = styled.div`
   margin: auto;
   box-sizing: border-box;
   display: grid;
-  grid-template-columns: repeat(auto-fill, minmax(280px, 1fr));
+  grid-template-columns: repeat(auto-fill, minmax(260px, 1fr));
   @media (max-width: 1300px) {
-    grid-template-columns: repeat(auto-fill, minmax(300px, 1fr));
+    grid-template-columns: repeat(auto-fill, minmax(280px, 1fr));
   }
   @media (max-width: 1160px) {
-    grid-template-columns: repeat(auto-fill, minmax(400px, 1fr));
+    grid-template-columns: repeat(auto-fill, minmax(380px, 1fr));
+  }
+  gap: 30px;
+`;
+
+const NoList = styled.div`
+  background-color: #f9f9f9;
+  width: 1136px;
+  height: 500px;
+  margin: 40px auto 80px;
+  border-radius: 20px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-flow: column;
+  h1 {
+    margin: 20px 0 0;
   }
 `;
 
