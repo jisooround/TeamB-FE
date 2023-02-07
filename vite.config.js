a65e6eba13e64faca3656a227f3f91b4c1efda35
mineclover <mineclover@naver.com>
2023-02-07 20:19:52 +0900

    feat : https

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,13 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import { resolve } from 'path';
+import basicSsl from '@vitejs/plugin-basic-ssl';
+import dns from 'dns';
+dns.setDefaultResultOrder('verbatim');
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), basicSsl()],
   resolve: {
     alias: [
       { find: '@', replacement: resolve(__dirname, 'src') },
@@ -16,4 +19,5 @@ export default defineConfig({
       { find: '@utils', replacement: resolve(__dirname, 'src/utils') },
     ],
   },
+  server: { https: true, host: 'localhost' },
 });
