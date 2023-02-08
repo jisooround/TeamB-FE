ea008688ee42bfa62668491114b62bfc3519cf89
jisooround <9utesleben@gmail.com>
2023-02-08 19:52:25 +0900

    Feat : Link to detail

--- src/components/search/searchCard.jsx
+++ src/components/search/searchCard.jsx
@@ -3,30 +3,24 @@ import { AiTwotoneStar } from 'react-icons/ai';
 import { MdImageNotSupported } from 'react-icons/md';
 import WishIcon from '@components/common/wishIcon/WishIcon';
 import styles from './SearchCard.module.scss';
-import { Link } from 'react-router-dom';
 
 const SearchCard = ({ list }) => {
   return (
     <div className={styles.card}>
       <div className={styles.image}>
-        <Link to={'/detail/' + list.contentid}>
-          {list.firstimage ? (
-            <img src={list.firstimage} alt={list.title + ' 사진'} />
-          ) : (
-            <div className={styles.noImage}>
-              <MdImageNotSupported size="84" color="#fff" />
-              <p>No Image</p>
-            </div>
-          )}
-        </Link>
+        {list.firstimage ? (
+          <img src={list.firstimage} alt={list.title + ' 사진'} />
+        ) : (
+          <div className={styles.noImage}>
+            <MdImageNotSupported size="84" color="#fff" />
+            <p>No Image</p>
+          </div>
+        )}
         <WishIcon className={styles.wishIcon} list={list} isSearch={true} />
       </div>
-
       <div className={styles.info}>
         <div className={styles.titleWrap}>
-          <Link to={'/detail/' + list.contentid}>
-            <p className={styles.title}>{list.title}</p>
-          </Link>
+          <p className={styles.title}>{list.title}</p>
           <p className={styles.rate}>
             <AiTwotoneStar className={styles.rateIcon} />
             {list.rate}
